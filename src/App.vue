d05359cd61ccdc37ece9ed75582c89a9431007cc
Ulla Volke <ulla.volke@khk.ee>
2018-10-11 11:33:49 +0300

    grammide teisendus lisatud

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   
+    <Converter/>
+  </div>
+</template>
+
+<script>
+import Converter from './components/Converter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Converter
+  }
+}
+</script>
+
+<style>
+
+</style>
